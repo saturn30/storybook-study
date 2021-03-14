b0f2e7e5d7f8b5040ef8fd748e910f930449b33c
soso01 <6125024@gmail.com>
2021-03-14 20:26:18 +0900

    edit style

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ export default function Button(props) {
   const { onClick, children } = props
   return (
     <button
-      style={{ width: 100, padding: 10, borderRadius: 20, backgroundColor: "yellow" }}
+      style={{ width: 100, padding: 10, borderRadius: 20, backgroundColor: "lavender" }}
       onClick={onClick}
     >
       {children}
