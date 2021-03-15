3b4958da8408626c0e28c29d3a0a242927cf28d3
soso01 <6125024@gmail.com>
2021-03-15 09:02:17 +0900

    edit padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ export default function Button(props) {
     <button
       style={{
         width: 100,
-        padding: 10,
+        padding: 15,
         borderRadius: 20,
         backgroundColor: "purple",
         color: "white",
